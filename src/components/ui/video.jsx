161cc456d96f327807c4c7eb9e0eed3f7e755c17
Ilca Almeida <ilcalmeida92@gmail.com>
2025-08-07 15:08:47 -0300

    fix: local do vídeo para src

--- src/components/ui/video.jsx
+++ src/components/ui/video.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import video from './../../../src/assets/videos/EM.webm';
 
 function Video() {
   return (
     <div className="aspect-w-16 aspect-h-16">
       <video
         className="w-full h-full object-cover"
-        src="EM.webm"
+        src={video}
         autoPlay
         loop
         muted
