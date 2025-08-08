e2e67718ba225aacbfefe60b2fcd459e56de7d9c
Ilca Almeida <ilcalmeida92@gmail.com>
2025-08-07 22:51:08 -0300

    deploy: segundo teste

--- src/App.jsx
+++ src/App.jsx
@@ -107,7 +107,7 @@ function App() {
 
   return (
     <>
-    {/* teste para deploy */}
+    {/* teste para deploy  2*/}
       <Helmet>
         <title>Emadeirando - Móveis Planejados de Qualidade</title>
         <meta name="description" content="Transforme seus ambientes com móveis planejados exclusivos da Emadeirando. Qualidade, design e funcionalidade para sua casa dos sonhos." />
